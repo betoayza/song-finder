c08af3b35fb7f2fdd1caba10481d7e592ead0c5b
Alberto <aaayza@gmail.com>
2022-05-28 17:11:57 -0300

    first commit

---
+++ src/components/Message.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Message.css';
+
+const Message = () => {
+  return (
+    <>
+        <div id="error-div">
+           <h2>An error happened!</h2>  
+        </div>
+    </>
+  )
+}
+
+export default Message;
